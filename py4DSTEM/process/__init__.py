31401f96eca07bb4982f538697521e6ec6f52c0b
serinlee1065 <serinl@stanford.edu>
2025-04-29 09:18:15 -0700

    Adding cluster class

--- py4DSTEM/process/__init__.py
+++ py4DSTEM/process/__init__.py
@@ -7,3 +7,5 @@ from py4DSTEM.process import utils
 from py4DSTEM.process import classification
 from py4DSTEM.process import diffraction
 from py4DSTEM.process import wholepatternfit
+
+from py4DSTEM.process.utils import Cluster
